73b68bdcb6d872b8c004038cce501d643365e256
Tiago Ribeiro <tiagor@btiestrategica.com.br>
2022-08-10 15:40:23 -0300

    add tratativas para comandos, e promisses para as funcoes de cmd

--- index.js
+++ index.js
@@ -1,48 +1,78 @@
 const express = require('express')
-const {exec} = require('child_process')
+const { exec } = require('child_process')
 require('dotenv/config')
 
 const app = express()
 
-app.post('/gitlab', (req, res)=>{
-	// Valida o token enviado
-	if(req.headers["x-gitlab-token"] !== process.env.GitToken){
-		res.status(401).send("Conexão recusada")
-		return
-	}
+app.use(express.json())
 
-	// Tratativa para caso demore mais de 5s a requisição
-	let timeout = setTimeout(()=>{
-		res.send("Timeout detectado")
-		timeout = false
-	}, 5 * 1000)
+app.post('/gitlab', (req, res) => {
+    // Valida o token enviado
+    if (req.headers["x-gitlab-token"] !== process.env.GitToken) {
+        res.status(401).send("Conexão recusada")
+        return
+    }
 
-	// Executa o comando CMD
-	exec("autoPull.cmd", (error, stdout, stderr)=>{
-		if(error){
-			console.log("erro =>", error)
-			if(timeout){				
-				res.status(500).json({
-					msg: "Erro ao realizar o pull",
-					error: error
-				})
-				clearTimeout(timeout)
-			}
-			return
-		}
-		console.log("stdout =>", stdout)
-		console.log("stderr =>", stderr)
-		if(timeout){
-			res.send("Sucesso")
-			clearTimeout(timeout)
-		}
-	})
+    // Executa os comandos CMD
+    executarPull().then((result) => {
+        let { status, body } = result
+        if (status == 500) {
+            res.status(status).json(body)
+            return
+        }
+        if (req.body.commits[0].title == "Download npm package") {
+            execNpmInstall().then((resultInstall) => {
+                let { status, body } = resultInstall
+                res.status(status).json(body)
+            })
+            return
+        }
+        res.status(status).json(body)
+    })
 })
 
-app.get('/', (req, res)=>{
-	res.send("olá mundo")
+app.get('/', (req, res) => {
+    res.send("olá mundo")
 })
 
-app.listen(3001, ()=>{
-	console.log(new Date().toLocaleString('pt-br'), "servidor aberto na url http://localhost:3001")
+app.listen(3001, () => {
+    console.log(new Date().toLocaleString('pt-br'), "servidor aberto na url http://localhost:3001")
 })
+
+function executarPull() {
+    return new Promise(resolve => {
+        exec("autoPull.cmd", (error, stdout, stderr) => {
+            if (error) {
+            	console.log(new Date().toLocaleString('pt-br'), "Erro >>>", error)
+                resolve({
+                    status: 500,
+                    body: "Ocorreu um erro ao executar autoPull.cmd"
+                })
+                return
+            }
+            resolve({
+                status: 200,
+				body: "Sucesso executando autoPull.cmd"
+            })
+        })
+    })
+}
+
+function execNpmInstall() {
+    return new Promise(resolve => {
+        exec("installPackages.cmd", (error, stdout, stderr) => {
+            if (error) {
+            	console.log(new Date().toLocaleString('pt-br'), "Erro >>>", error)
+                resolve({
+                    status: 500,
+                    body: "Ocorreu um erro ao executar installPackages.cmd"
+                })
+                return
+            }
+            resolve({
+                status: 200,
+				body: "Sucesso executando installPackages.cmd"
+            })
+        })
+    })
+}
